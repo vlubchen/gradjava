f1df7b77c1dacec9fcb9e4c41fd0e080b1f44f03
vlubchen <vlubchen@gmail.com>
2022-12-05 23:43:40 +0400

    Initial

---
+++ src/main/java/ru/vlubchen/gradjava/Main.java
@@ -0,0 +1,14 @@
+package ru.vlubchen.gradjava;
+
+/**
+ * User:vlubchen
+ * Date:05.12.2022
+ *
+ * @link https://github.com/vlubchen/gradjava
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("It's beginning of my graduation project!");
+    }
+}
