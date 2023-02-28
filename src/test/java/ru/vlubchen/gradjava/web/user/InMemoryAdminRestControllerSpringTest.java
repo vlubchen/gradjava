334fa6389db7c5c5fbb83ce8be089180ceac4a5f
Vadim <vlubchen@gmail.com>
2023-02-28 10:21:31 +0400

    Add_spring_test

---
+++ src/test/java/ru/vlubchen/gradjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -0,0 +1,39 @@
+package ru.vlubchen.gradjava.web.user;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.vlubchen.gradjava.repository.inmemory.InMemoryUserRepository;
+import ru.vlubchen.gradjava.util.exception.NotFoundException;
+
+import static ru.vlubchen.gradjava.UserTestData.NOT_FOUND;
+import static ru.vlubchen.gradjava.UserTestData.USER_ID;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringRunner.class)
+public class InMemoryAdminRestControllerSpringTest {
+    @Autowired
+    private AdminRestController controller;
+    @Autowired
+    private InMemoryUserRepository repository;
+
+    @Before
+    public void setUp() {
+        repository.init();
+    }
+
+    @Test
+    public void delete() {
+        controller.delete(USER_ID);
+        Assert.assertNull(repository.get(USER_ID));
+    }
+
+    @Test
+    public void deleteNotFound() {
+        Assert.assertThrows(NotFoundException.class, () -> controller.delete(NOT_FOUND));
+    }
+}
