5f7b3e85ebe906b3f6c962504fdc03272497d0fd
vlubchen <vlubchen@gmail.com>
2023-03-09 23:12:57 +0400

    Fix_JdbcDishRepository_tests

--- src/main/java/ru/vlubchen/gradjava/util/RestaurantUtil.java
+++ src/main/java/ru/vlubchen/gradjava/util/RestaurantUtil.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class RestaurantUtil {
 
     public static final List<Restaurant> restaurants = Arrays.asList(
-            new Restaurant("Sholly", "+7(8512)51-60-57", "ул. Урицкого, д.3", "upravl_sholi@am-house.ru"),
-            new Restaurant("Izba", "+7(8512)51-81-91", "ул. Красная Набережная, д.8", "izba2012@inbox.ru"),
-            new Restaurant("BeerHouse", "+7(8512)54-72-72", "ул. Савушкина, д.38", "pivdom@am-house.ru")
+            new Restaurant(100003,"Sholly", "+7(8512)51-60-57", "ул. Урицкого, д.3", "upravl_sholi@am-house.ru"),
+            new Restaurant(100004, "Izba", "+7(8512)51-81-91", "ул. Красная Набережная, д.8", "izba2012@inbox.ru"),
+            new Restaurant(100005,"BeerHouse", "+7(8512)54-72-72", "ул. Савушкина, д.38", "pivdom@am-house.ru")
     );
 }
--- src/test/java/ru/vlubchen/gradjava/DishTestData.java
+++ src/test/java/ru/vlubchen/gradjava/DishTestData.java
@@ -16,27 +16,27 @@ public class DishTestData {
     public static final int NOT_FOUND = 10;
     public static final int DISH1_ID = START_SEQ + 6;
 
-    public static final Dish dish1 = new Dish(DISH1_ID, LocalDate.of(2023, Month.JANUARY, 9), restaurants.get(0), "Грибной суп", 250);
-    public static final Dish dish2 = new Dish(DISH1_ID + 4, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(0), "Стейк", 800);
-    public static final Dish dish3 = new Dish(DISH1_ID + 5, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(0), "Винегрет", 150);
-    public static final Dish dish4 = new Dish(DISH1_ID + 6, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(0), "Компот", 50);
-    public static final Dish dish5 = new Dish(DISH1_ID + 7, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(1), "Борщ", 400);
-    public static final Dish dish6 = new Dish(DISH1_ID + 8, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(1), "Плов", 500);
-    public static final Dish dish7 = new Dish(DISH1_ID + 9, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(1), "Оливье", 150);
-    public static final Dish dish8 = new Dish(DISH1_ID + 10, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(1), "Чай", 50);
-    public static final Dish dish9 = new Dish(DISH1_ID + 11, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(2), "Грибной суп", 250);
-    public static final Dish dish10 = new Dish(DISH1_ID + 12, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(2), "Стейк", 800);
-    public static final Dish dish11 = new Dish(DISH1_ID + 13, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(2), "Винегрет", 150);
-    public static final Dish dish12 = new Dish(DISH1_ID + 14, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(2), "Чай", 50);
-    public static final Dish dish13 = new Dish(DISH1_ID + 15, LocalDate.of(2023, Month.MARCH, 10), restaurants.get(1), "Борщ", 400);
-    public static final Dish dish14 = new Dish(DISH1_ID + 16, LocalDate.of(2023, Month.MARCH, 10), restaurants.get(1), "Плов", 500);
-    public static final Dish dish15 = new Dish(DISH1_ID + 17, LocalDate.of(2023, Month.MARCH, 10), restaurants.get(1), "Оливье", 150);
-    public static final Dish dish16 = new Dish(DISH1_ID + 18, LocalDate.of(2023, Month.MARCH, 10), restaurants.get(1), "Чай", 50);
+    public static final Dish dish1 = new Dish(DISH1_ID, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(0), "Грибной суп", 250);
+    public static final Dish dish2 = new Dish(DISH1_ID + 1, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(0), "Стейк", 800);
+    public static final Dish dish3 = new Dish(DISH1_ID + 2, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(0), "Винегрет", 150);
+    public static final Dish dish4 = new Dish(DISH1_ID + 3, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(0), "Компот", 50);
+    public static final Dish dish5 = new Dish(DISH1_ID + 4, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(1), "Борщ", 400);
+    public static final Dish dish6 = new Dish(DISH1_ID + 5, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(1), "Плов", 500);
+    public static final Dish dish7 = new Dish(DISH1_ID + 6, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(1), "Оливье", 150);
+    public static final Dish dish8 = new Dish(DISH1_ID + 7, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(1), "Чай", 50);
+    public static final Dish dish9 = new Dish(DISH1_ID + 8, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(2), "Грибной суп", 400);
+    public static final Dish dish10 = new Dish(DISH1_ID + 9, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(2), "Удон", 600);
+    public static final Dish dish11 = new Dish(DISH1_ID + 10, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(2), "Греческий", 200);
+    public static final Dish dish12 = new Dish(DISH1_ID + 11, LocalDate.of(2023, Month.MARCH, 9), restaurants.get(2), "Чай", 50);
+    public static final Dish dish13 = new Dish(DISH1_ID + 12, LocalDate.of(2023, Month.MARCH, 10), restaurants.get(1), "Борщ", 400);
+    public static final Dish dish14 = new Dish(DISH1_ID + 13, LocalDate.of(2023, Month.MARCH, 10), restaurants.get(1), "Плов", 500);
+    public static final Dish dish15 = new Dish(DISH1_ID + 14, LocalDate.of(2023, Month.MARCH, 10), restaurants.get(1), "Оливье", 150);
+    public static final Dish dish16 = new Dish(DISH1_ID + 15, LocalDate.of(2023, Month.MARCH, 10), restaurants.get(1), "Чай", 50);
 
-    public static final List<Dish> dishes = Arrays.asList(dish1, dish2, dish3, dish3, dish5, dish6, dish7, dish8, dish9, dish10, dish11, dish12, dish13, dish14, dish15, dish16);
+    public static final List<Dish> dishes = Arrays.asList(dish1, dish2, dish3, dish4, dish5, dish6, dish7, dish8, dish9, dish10, dish11, dish12, dish13, dish14, dish15, dish16);
 
     public static Dish getNew() {
-        return new Dish(null, LocalDate.of(2023, Month.JANUARY, 15), restaurants.get(0), "Новое блюдо", 500);
+        return new Dish(null, LocalDate.of(2023, Month.JANUARY, 9), restaurants.get(0), "Новое блюдо", 500);
     }
 
     public static Dish getUpdated() {
